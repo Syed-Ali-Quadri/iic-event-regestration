62d2312ec1294d76937c57562a731fd8f6fda163
Syed Ali Quadri <syedaliquadri@Syeds-MacBook-Pro.local>
2026-04-22 13:13:32 +0530

    feat: replace user endpoint with verification route to handle registration check-ins

---
+++ app/api/verify/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import main from "@/config/db";
+
+import Register from "@/model/register.model";
+
+export async function GET(request: NextRequest) {
+    try {
+        await main();
+
+        const { slug } = await request.json();
+
+        if (slug.length < 1 && slug.length < 20) {
+            return NextResponse.json(
+                { error: "Invalid slug" },
+                { status: 400 }
+            );
+        }
+
+        const existUser = await Register.findOne({ slug });
+
+        if (!existUser) {
+            return NextResponse.json(
+                { error: "Slug not found" },
+                { status: 404 }
+            );
+        }
+
+        existUser.checkedIn = true;
+        existUser.checkedInAt = new Date();
+
+        await existUser.save();
+
+        return NextResponse.json(
+            { existUser },
+            { status: 200 }
+        );
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to connect to database" },
+            { status: 500 }
+        );
+    }
+}
